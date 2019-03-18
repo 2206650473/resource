2b93fdb6060e3a1a1a8dcd9392b3c86ace53ab5c
2206650473 <15914651471@163.com>
2019-03-19 00:20:58 +0800

    第8次修改

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fun() {
-    if (confirm("你今年多少岁？")) {
-        document.getElementById("message").innerText = "我想看见她哭笑不得？"
-    }else{
-        document.getElementById('message').innerText = "那你就笑吧，有一天我笑给你看。"
+    if (confirm("你喜欢笑我吗？")) {
+        document.getElementById("message").innerText = "那你就笑吧，有一天我笑给你看。"
+    } else {
+        document.getElementById('message').innerText = "恭喜你，算你还识相？"
     }
 }
